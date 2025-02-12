eed90e15d71192cb9caf86c0b3badb198596b278
Meph1sto666 <phentragon@gmx.de>
2025-02-12 10:27:55 +0100

    feat(items): added further item support
    cleansed public/images moving all icons to a public/images/items

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -6,7 +6,7 @@
 <div class={`${event}-tooltip event_${event} tooltip`}>
     {#each Object.entries(items) as [stage, item]}
         <div class="tooltip-item">
-            <img class="tooltip-item-icon" src={`images/materials/${item}.png`} alt={item} />
+            <img class="tooltip-item-icon" src={`images/items/${item}.png`} alt={item} />
             <span class="tooltip-item-stage">{stage}</span>
         </div>
     {/each}
